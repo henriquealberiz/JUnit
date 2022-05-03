ff1c6bd0889494d86173a7030b15c9cd1726a513
henriquealberiz <100207162+henriquealberiz@users.noreply.github.com>
2022-05-03 11:53:18 -0300

    Update testeSoma.java

--- testeSoma.java
+++ testeSoma.java
@@ -8,8 +8,14 @@ public class testeSoma {
 	public void test() {
 		UnitTesting obj1 = new UnitTesting();
 		int output_q = obj1.soma(2,2);
-		//testa a sa�da 
+		//testa a saída 
 		assertEquals(4,output_q);
 	}
-
+	@Test
+	public void test2() {
+		UnitTesting obj2 = new UnitTesting();
+		int output_q = obj2.soma(-2,1);
+		//testa a saída 
+		assertEquals(-1,output_q);
+	}
 }
