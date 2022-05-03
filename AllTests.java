9e86d8c4faa1ee476dce35c40700ce4212cb52f6
henriquealberiz <100207162+henriquealberiz@users.noreply.github.com>
2022-05-03 11:35:23 -0300

    Update AllTests.java

--- AllTests.java
+++ AllTests.java
@@ -3,8 +3,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllTests.class, testeArray.class, testeQuadrado.class })
+@SuiteClasses({ AllTests.class, testeArray.class, testeDivisao.class, testeMultiplicacao.class, testeQuadrado.class,
+		testeSoma.class, testeSubtracao.class })
 
 public class AllTests {
 
 }
+
